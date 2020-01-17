548dba0593c857c0c148caf4ba2e0c033506b1b9
sahaa46 <49682103+sahaa46@users.noreply.github.com>
2020-01-17 20:42:18 +0100

    Add files via upload

--- Ad CSS/js/script.js
+++ Ad CSS/js/script.js
@@ -23,6 +23,8 @@ function reverse() {
 }
 */
 
+
+/*
 var boje = []
 
 var kockica = {
@@ -34,7 +36,7 @@ var kockica = {
         display.innerHTML = randomNum;
         kockica.promjeniBoju(randomNum);
     },
-    promjeniBoju:function (index) {
+    promjeniBoju: function (index) {
         console.log('koc');
         var display = document.getElementById('kockica');
         display.style.backgroundColor = kockica.boje[index]
@@ -42,3 +44,78 @@ var kockica = {
 };
 
 kockica.baci();
+*/
+
+
+
+/*
+function clearForm() {
+    document.getElementById('signup1').reset();
+}
+*/
+
+var users = [];
+
+function signUp() {
+    var user = {
+        name: getVal('name'),
+        lastname: getVal('lastname'),
+        email1: getVal('email1'),
+        password1: getVal('password1'),
+        username: getVal('username'),
+        img: getVal('img')
+    };
+    users.push(user);
+    console.log(users);
+    confirm('Continue?')
+    var hideSignUp = document.getElementById('signup');
+    hideSignUp.style.display = 'none'
+    var returnLogin = document.getElementById('form');
+    returnLogin.style.display = 'block';
+}
+
+
+function getVal(id) {
+    return document.getElementById(id).value;
+}
+
+
+function validate() {
+    for (var i = 0; i < users.length; i++) {
+        var emailValue = document.getElementById('email').value;
+        var passValue = document.getElementById('password').value;
+
+        if (emailValue == users[i].email1 && passValue == users[i].password1) {
+            var loginform = document.getElementById('form');
+            loginform.style.display = 'none';
+            var homePage = document.getElementById('hideh1');
+            homePage.style.display = 'block';
+            console.log('Welcome!');
+        }
+        else {
+            var errMsg = document.getElementById('err-msg');
+            errMsg.style.display = 'block';
+            console.log('Wrong email/pw');
+        }
+    }
+}
+
+function loginOnEnter(e) {
+    if (e.keyCode == 13) {
+        validate();
+    }
+}
+
+function showLogin() {
+    var showLogin = document.getElementById('form');
+    showLogin.style.dispaly = 'block';
+}
+
+function logout() {
+    var homePage = document.getElementById('hideh1');
+    homePage.style.display = 'none';
+    showLogin();
+}
+
+
+
