2bf38be75ad894b13e7a6c81e076e419def6ef5c
sahaa46 <49682103+sahaa46@users.noreply.github.com>
2020-01-31 20:36:35 +0100

    Add files via upload

--- Ad CSS/Jela/js/script.js
+++ Ad CSS/Jela/js/script.js
@@ -51,3 +51,43 @@ function transfer() {
     }
 }
 */
+
+function brojcanik(id) {
+    var sviSegmenti = [];
+    var clock = document.getElementById(id);
+    var wrapper = document.createElement('div');
+    wrapper.className = 'first';
+    for (var i = 1; i < 8; i++) {
+        var segment = document.createElement('div');
+        segment.className = 'segment s' + i;
+        wrapper.appendChild(segment);
+        sviSegmenti.push(segment)
+    }
+    clock.appendChild(wrapper);
+
+    function ukljuci(segment) {
+        segment.classList.remove('ugasen')
+    }
+
+    function postaviVrijeme(broj) {
+        for (let i = 0; i < sviSegmenti.length; i++) {
+            sviSegmenti[i].classList.add('ugasen')
+        }
+        switch (broj) {
+            case 1:
+                ukljuci(sviSegmenti[2]);
+                ukljuci(sviSegmenti[5]);
+                break;
+        }
+    }
+    return {
+        setTime: postaviVrijeme
+    }
+}
+
+var br1 = brojcanik('clock');
+br1.setTime(1);
+
+
+
+
