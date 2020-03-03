debadb52fdbe86c96155e51407eb5512b9cc5e73
sahaa46 <49682103+sahaa46@users.noreply.github.com>
2020-03-03 20:35:35 +0100

    Add files via upload

---
+++ UPDATE/VueJS/js/script.js
@@ -0,0 +1,36 @@
+/*
+Vue.component('todo-item', {
+    template: '<li style="color:red;">{{todo.text}}</li>',
+    props: ['todo']
+});
+
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        msg: 'Emin Sahat',
+        titleMsg: 'Bravo macko',
+        imgUrl: 'https://vuejs.org/images/logo.png',
+        show: false,
+        todos: [
+            { text: 'JavaScript najbolji jezik' },
+            { text: 'Vue.js najbolji framwork' },
+            { text: 'HTML je beton' }
+        ],
+        firstName: 'Emin'
+    },
+    methods: {
+        showImg: function (evt) {
+            console.log(evt);
+            this.show = !this.show;
+        },
+        logData: function () {
+            console.log(this.firstName);
+
+        }
+    }
+});
+*/
+
+
+
